17dc006ea7001d13a98119bf06df7bb4510b1124
xuhaoyuan <38752437@qq.com>
2026-04-15 14:50:16 +0800

    Initial Commit

---
+++ ClaudeBridgeApp/ClaudeBridgeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClaudeBridgeAppApp.swift
+//  ClaudeBridgeApp
+//
+//  Created by 许浩渊 on 2026/4/15.
+//
+
+import SwiftUI
+
+@main
+struct ClaudeBridgeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
